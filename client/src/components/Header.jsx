0893e10098a11325a9e8a762eef8bf2c084726fe
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-12-29 13:26:34 +0100

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,17 +6,17 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Estate</span>
-            <span className="text-slate-700">MarketPlace</span>
+            <span className="text-slate-500">Sahand</span>
+            <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center ">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-inherit focus:outline-none w-24 sm:w-64"
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-slate-500" />
+          <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-3">
           <Link to="/">
